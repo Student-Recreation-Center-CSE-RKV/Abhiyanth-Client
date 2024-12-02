c8606d0e0ba5894852959c10791648c0eb580dbb
revanthkumarJ <jrevanth101@gmail.com>
2024-12-03 00:19:53 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Events from './pages/Events';
+import Stalls from './pages/Stalls';
+import ResponsiveAppBar from './components/Navbar';
 
 function App() {
   return (
-    <>
-    Abhiyanth
-    </>
+    <BrowserRouter>
+      <ResponsiveAppBar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/events" element={<Events/>} />
+        <Route path="/stalls" element={<Stalls/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
