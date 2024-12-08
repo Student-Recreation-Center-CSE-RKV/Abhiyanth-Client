848241fbf14c187b0340dac8b641b818ed396a90
rajeswari-Machina <rajeswarimachina02@gmail.com>
2024-12-08 15:39:48 +0530

    Added NavBarLayout For Admin

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Events from './pages/Events';
 import Stalls from './pages/Stalls';
-import ResponsiveAppBar from './components/Navbar';
+//import ResponsiveAppBar from './components/Navbar';
+import NavBarLayout from './components/NavBarLayout';
+import EventCalendar from './components/EventCalendar';
 
 function App() {
   return (
     <BrowserRouter>
-      <ResponsiveAppBar/>
+    <NavBarLayout  />
+
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>} />
         <Route path="/events" element={<Events/>} />
         <Route path="/stalls" element={<Stalls/>} />
+        <Route path="/event-calendar" element={<EventCalendar/>} />
       </Routes>
     </BrowserRouter>
   );
