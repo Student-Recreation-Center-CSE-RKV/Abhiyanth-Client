f99d1805e701417482e8ed5aee582c5b7628107e
revanthkumarJ <jrevanth101@gmail.com>
2024-12-20 10:11:25 +0530

    refined

--- src/App.js
+++ src/App.js
@@ -4,12 +4,9 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Events from "./pages/Events";
 import Stalls from "./pages/Stalls";
-//import ResponsiveAppBar from './components/Navbar';
-import NavBarLayout from "./components/NavBarLayout";
 import EventCalendar from "./components/EventCalendar";
 import Admin from "./pages/Admin";
 import Main from "./pages/Main";
-import Sponsors from "./pages/Sponsers";
 
 function App() {
 	return (
@@ -22,7 +19,6 @@ function App() {
 					<Route path="/stalls" element={<Stalls />} />
 					<Route path="/event-calendar" element={<EventCalendar />} />
 				</Route>
-				<Route path="/sponsors" element={<Sponsors />} />
 				<Route path="/admin" element={<Admin />}></Route>
 			</Routes>
 		</BrowserRouter>
