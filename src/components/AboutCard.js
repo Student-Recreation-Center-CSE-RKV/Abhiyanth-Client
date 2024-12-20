127ab9ef1589e814d8f96121cdae164e5b04afce
revanthkumarJ <jrevanth101@gmail.com>
2024-12-20 10:10:06 +0530

    moved Sponsers componenet from App.js to Home

---
+++ src/components/AboutCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import "./Card.css";
+
+const AboutCard = ({ imageSrc, title, subtitle }) => {
+  return (
+    <div className="custom-card">
+      {/* <img src={imageSrc} alt={title} className="card-image" />
+      <h3 className="card-title">{title}</h3>
+      <p className="card-subtitle">{subtitle}</p> */}
+    </div>
+  );
+};
+
+export default AboutCard;
