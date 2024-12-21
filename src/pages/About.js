8ae17a44d4156c9de830cc04ce9faabf8f5bf3e1
revanthkumarJ <jrevanth101@gmail.com>
2024-12-21 09:52:58 +0530

    css fix

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "../styles/about.css";  // Make sure this is the correct path
 import PreEventsCard from '../components/about/preEvents';
+import StatsComponent from "../components/about/StatsComponent"
 
 export default function About() {
   return (
     <div className="gif-background">
       <p style={{ fontSize: "2rem", color: "pink" }}>About</p>
       <PreEventsCard />
+      <StatsComponent/>
     </div>
   );
 }
