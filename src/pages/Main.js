c2750d5398915503fa40d7d68891dc6da09602d4
revanthkumarJ <jrevanth101@gmail.com>
2024-12-21 09:40:21 +0530

    about background done

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,15 +1,33 @@
-import React from 'react'
-
+import React from 'react';
 import Header from '../components/Header';
 import { Outlet } from "react-router-dom";
-import Footer from '../components/Footer'
+import Footer from '../components/Footer';
 
 export default function Main() {
+  const styles = {
+    mainContainer: {
+      display: 'flex',
+      flexDirection: 'column',
+      minHeight: '100vh', 
+    },
+    contentContainer: {
+      flex: 1, 
+      paddingBottom: '100px', 
+    },
+    footerContainer: {
+      marginTop: 'auto', 
+    }
+  };
+
   return (
-    <div>
-        <Header/>
+    <div style={styles.mainContainer}>
+      <Header />
+      <div style={styles.contentContainer}>
         <Outlet />
-        <Footer/>
+      </div>
+      <div style={styles.footerContainer}>
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
