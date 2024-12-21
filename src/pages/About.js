d6a90b9e47cdc55ff2886d649f75523e04867857
Achyutha Vikaram <achyuthavikaram@gmail.com>
2024-12-21 16:51:46 +0530

    changed about.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,9 @@ import AimVision from "../components/about/AimVision";
 export default function About() {
 	return (
 		<div className="gif-background">
+      <div>
 			<AimVision />
+      </div>
 			<StatsComponent />
 			<PreEventsCard />
 			<DeptEvents />
