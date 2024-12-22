4e5fa594e460931bfd472791f5b3e2150e787cac
rajeswari-Machina <rajeswarimachina02@gmail.com>
2024-12-22 10:36:41 +0530

    Added responsiveness to header for small screens

--- src/components/general/Header.js
+++ src/components/general/Header.js
@@ -6,16 +6,84 @@ import Button from '@mui/material/Button';
 import { motion } from "motion/react";
 import { miniAbhiyanthLogo } from '../../assets/images/index';
 import { useNavigate, useLocation } from 'react-router-dom';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MenuIcon from '@mui/icons-material/Menu';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { Typography } from '@mui/material';
 
-function Header() {
-  const textStyles = {
+const styles ={
+  menubar:{
+    display: "flex",
+    width:"100%",
+    flexDirection: "row",
+    gap: "20px",
+    alignItems: "center",
+    justifyContent: "space-around",
+    height: "40px"
+  },
+  textStyles : {
     fontFamily: "Audiowide",
     fontWeight: "400",
     fontStyle: "normal",
     color: "white",
     textTransform: "none", 
-    fontSize: "17px"
+    fontSize: "17px"},
+  
+  }
+function BasicMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
   };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between", width: "100%" }}>
+      <nav style={{ display: "flex", alignItems: "center" }}>
+        <img src={miniAbhiyanthLogo} alt="A" style={{ width: "40px", height: "40px", marginRight: "10px" }} />
+        <Typography sx={styles.textStyles}>Abhiyanth</Typography>
+      </nav>
+      <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        sx={{ color: "white", background: "transparent", border: "none" }}
+      >
+        <MenuIcon />
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem onClick={handleClose}>Home</MenuItem>
+        <MenuItem onClick={handleClose}>Gallery</MenuItem>
+        <MenuItem onClick={handleClose}>Sponsors</MenuItem>
+        <MenuItem onClick={handleClose}>About</MenuItem>
+      </Menu>
+    </div>
+  );
+}
+
+function Header() {
+
 
   const navigate = useNavigate();
   const location = useLocation();
@@ -38,23 +106,15 @@ function Header() {
   return (
     <Box>
       <AppBar position="fixed" sx={{ borderRadius: "0px 0px 30px 30px", backgroundColor: "rgb(137,112,162,0.8)", zIndex: "2" }}>
-        <Toolbar sx={{
-          display: "flex",
-          width:"100%",
-          flexDirection: "row",
-          gap: "20px",
-          alignItems: "center",
-          justifyContent: "space-around",
-          height: "40px"
-        }}>
+        <Toolbar sx={styles.menubar}>
           <nav>
-            <Button variant="text" sx={textStyles} onClick={() => navigate("/")}>
+            <Button variant="text" sx={styles.textStyles} onClick={() => navigate("/")}>
               Home
             </Button>
             {isSelected === 1 && <motion.div layoutId='underline' style={{ boxShadow: "0px 4px 2px 1px white", width: "100%" }} />}
           </nav>
           <nav>
-            <Button variant="text" sx={textStyles} onClick={() => navigate("/gallery")}>
+            <Button variant="text" sx={styles.textStyles} onClick={() => navigate("/gallery")}>
               Gallery
             </Button>
             {isSelected === 2 && <motion.div layoutId='underline' style={{ boxShadow: "0px 4px 2px 1px white", width: "100%" }} />}
@@ -65,13 +125,13 @@ function Header() {
             </div>
           </nav>
           <nav>
-            <Button variant="text" sx={textStyles} onClick={() => navigate("/sponsors")}>
+            <Button variant="text" sx={styles.textStyles} onClick={() => navigate("/sponsors")}>
               Sponsors
             </Button>
             {isSelected === 3 && <motion.div layoutId='underline' style={{ boxShadow: "0px 4px 2px 1px white", width: "100%" }} />}
           </nav>
           <nav>
-            <Button variant="text" sx={textStyles} onClick={() => navigate("/about")}>
+            <Button variant="text" sx={styles.textStyles} onClick={() => navigate("/about")}>
               About Us
             </Button>
             {isSelected === 4 && <motion.div layoutId='underline' style={{ boxShadow: "0px 4px 2px 1px white", width: "100%" }} />}
@@ -82,4 +142,14 @@ function Header() {
   );
 }
 
-export default Header;
+function ResponsiveMenu() {
+  const isSmallScreen = useMediaQuery('(max-width:650px)');
+
+  return (
+    <div>
+      {isSmallScreen ? <BasicMenu /> : <Header />}
+    </div>
+  );
+}
+
+export default ResponsiveMenu;
