e6fe7a323eb33ffdcc652f99f186e0ffb76cea03
revanthkumarJ <jrevanth101@gmail.com>
2024-12-31 22:52:22 +0530

    stagged changes

--- src/pages/Sponsers.js
+++ src/pages/Sponsers.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Sponser from '../components/home/Sponsers'
+import Brochure from '../components/sponsers/Broucher'
 
 export default function Sponsers() {
   return (
     <div style={{paddingTop:"40px"}}>
-      <Sponser />
+      <Brochure/>
     </div>
   )
 }
