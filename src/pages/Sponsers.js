adb8b5ad2b94bfdd6052abbae4f189c81fd55d1a
Achyutha Vikaram <achyuthavikaram@gmail.com>
2025-01-01 11:28:55 +0530

    added whats in abiyanth for you

--- src/pages/Sponsers.js
+++ src/pages/Sponsers.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Sponser from '../components/home/Sponsers'
 import Brochure from '../components/sponsers/Broucher'
+import WhatsInAbiyanth from '../components/sponsers/WhatsInAbiyanth'
+import GradientBorderComponent from '../components/general/GradientBorderComponet'
 
 export default function Sponsers() {
   return (
     <div style={{paddingTop:"40px"}}>
       <Brochure/>
+      <GradientBorderComponent/>
+      <WhatsInAbiyanth/>
     </div>
   )
 }
