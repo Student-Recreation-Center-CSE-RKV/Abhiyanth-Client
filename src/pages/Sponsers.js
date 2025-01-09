2bded35e2c79eff3e9929ca98ce58187566dda18
Achyutha Vikaram <achyuthavikaram@gmail.com>
2025-01-09 06:47:27 +0530

    some changes

--- src/pages/Sponsers.js
+++ src/pages/Sponsers.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import Brochure from '../components/sponsers/Broucher'
-import GradientBorderComponent from '../components/general/GradientBorderComponet'
-import WhatsAbhiyanth from '../components/sponsers/WhatsAbhiyanth'
+import React from "react";
+import Brochure from "../components/sponsers/Broucher";
+import GradientBorderComponent from "../components/general/GradientBorderComponet";
+import WhatsAbhiyanth from "../components/sponsers/WhatsAbhiyanth";
+import WhatsInAbiyanth from "../components/sponsers/WhatsInAbiyanth";
 
 export default function Sponsers() {
-  return (
-    <div>
-      <WhatsAbhiyanth/>
-      <GradientBorderComponent/>
-      <div style={{paddingTop:"40px"}}>
-      <Brochure />
-      </div>
-    </div>
-  )
+	return (
+		<div>
+			<WhatsAbhiyanth />
+			<GradientBorderComponent />
+
+			<Brochure />
+			<GradientBorderComponent />
+			<WhatsInAbiyanth />
+		</div>
+	);
 }
