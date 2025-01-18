aa07afacb8cedde430c16d288bda70c7dfd3c80f
THOTA-SRIKANTH <tsrikanth866@gmail.com>
2025-01-18 20:31:53 +0530

    Deleted unnecessary files  in admin branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Stalls from './pages/Stalls';
+
 import Admin from './pages/Admin';
-import Main from './pages/Main';
-import Gallery from './pages/Gallery';
-import Sponsers from './pages/Sponsers';
-import OurTeam from './pages/OurTeam';
-import EventDetails from './pages/EventDetails';
-import AllEvents from './components/eventDetails/allEvents';
+
 import ScrollToTop from './components/general/ScrollToTop';
-import ContactUs from './pages/ContactUs';
-import DeptWiseEvents from './components/technicalEvents/DeptWiseEvents.js'
-import TechnicalEvents from "./pages/TechnicalEvents.js"
-import RegisterVolunteer from "./pages/RegisterVolunteer.js"
-import TechEventDetails from './components/technicalEvents/techEventDetails.js';
-import StallShowcase from './components/stalls/StallShowCase.js';
 
 function App() {
-  
+
   return (
     <BrowserRouter>
       <ScrollToTop />
