69536264e2a43d5cdf5abaad81a82fb02dd58079
JILAKARA REVANTH KUMAR <146642552+revanthkumarJ@users.noreply.github.com>
2025-01-24 15:18:42 +0530

    Revert "payment and solved some merge issues (#142)"
    This reverts commit 5dc628651bc4f84475416fd40e990c48b85ff94e.

--- src/App.js
+++ src/App.js
@@ -28,9 +28,6 @@ import { toast, ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import { useState, useContext, createContext } from 'react';
 import { isInDb } from './api/users.js';
-import Payment from './pages/payment.js';
-import Cashfree from './components/CashFreeFold/cashFree.js';
-import RegisteredEvents from "./components/userprofile/RegisteredEvents.js"
 
 
 const provider = new GoogleAuthProvider();
@@ -54,6 +51,7 @@ function App() {
           <Route path="/events" element={<AllEvents />} />
           <Route path="/events/:id" element={<EventDetails />} />
           <Route path="/contactUs" element={<ContactUs/>} />
+          <Route path="/contactUs" element={<ContactUs />} />
           <Route path="/stalls" element={< Stalls />} />
           <Route path="/stalls/:id" element={<StallShowcase />} />
           <Route path="/technicalEvents" element={<TechnicalEvents />} />
@@ -63,9 +61,6 @@ function App() {
           <Route path="/projectShowCase" element={<ProjectsPage />} />
           <Route path='/news' element={<News/>}/>
           <Route path="/news/:id" element={<DetailedNews/>}/>
-          <Route path='/payment' element={<Cashfree/>}/>
-          <Route path="/profile" element={<Profile/>}/>
-          <Route path="/Registered" element={<RegisteredEvents/>}/>
         </Route>
         <Route path='auth/login' element={<SignInWrapper />} />
       </Routes>
