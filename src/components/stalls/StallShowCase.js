eaa427ee985c805ccbf3046aaa00377f164e4650
JILAKARA REVANTH KUMAR <146642552+revanthkumarJ@users.noreply.github.com>
2025-01-27 13:46:39 +0530

    Merge (#151)
    * updated About page department wise events
    
    * updated
    
    * commit
    
    * update
    
    * updated
    
    * updated
    
    * Merge branch 'payment'
    
    * updated
    
    * StallsPage Integration done

--- src/components/stalls/StallShowCase.js
+++ src/components/stalls/StallShowCase.js
@@ -60,6 +60,7 @@ const styles = {
 		flexDirection: "column",
 		justifyContent: "space-between",
 
+
 		margin: "1%",
 		width: "60%",
 		gap: "1",
@@ -106,6 +107,132 @@ function StallShowcase() {
 	if (stall == null) {
 		return <></>;
 	}
+
+
+function StallShowcase() {
+
+    const { id } = useParams();
+    const dispatch = useDispatch();
+      
+      const { stall, loading } = useSelector((state) => state.stalls);
+
+      const fetchStall = async () => {
+          
+          try {
+            
+            dispatch(getStallById(id))
+          } catch (error) {
+          } 
+        
+          };
+      
+        useEffect(() => {
+          
+            fetchStall();
+          
+          },[]);
+  
+  const isSmallScreen = useMediaQuery('(max-width:600px)');
+
+  if(stall==null)
+  {
+    return  <></>
+  }
+  return (
+    <Box sx={{margin:"6% 1% 1% 1%"}}>
+      <Box sx={styles.titleContainer}>
+        <Typography sx={styles.title}> 
+          {stall.name}
+        </Typography>
+        <motion.div
+        sx={{color:"white",fontSize:"20px",fontWeight:"bold"}}
+        animate={{ x:[-100,0,100] }} 
+        transition={{ duration: 5, repeat: Infinity, ease: "linear" }}
+        >
+          <Box sx={{display:"flex",gap:"10px",alignItems:"center"}}>
+          <img src={specialofferIcon} style={{height:"50px"}} alt="Offer"/>
+          { 
+            stall.offers &&
+            stall.offers.map((offer) => (
+              <>
+                <Typography sx={styles.subHeading}>
+                  {offer}
+                </Typography>
+                <img src={specialofferIcon} style={{height:"50px"}} alt="Offer"/>
+              </>
+            ))
+    }
+          </Box>
+        </motion.div>
+        <Box sx={{
+          ...styles.DescriptionContainer,
+          flexDirection: isSmallScreen ? "column" : "row"
+        }}>
+          {
+            isSmallScreen && (
+              <Box id="menu" sx={{width:"100%"}}>
+            <ImageCarousel images={[stall.image,stall.imageLeft,stall.imageRight]} />
+          </Box>
+            )
+          }
+          <Box sx={styles.subDescriptionContainer}>
+            <Box>
+              <Typography sx={styles.content}>
+                <Box sx={{color:"yellow",fontSize:"20px",fontWeight:"bold"}}>
+                  {stall.name}
+                </Box>
+                  {stall.main_description}
+              </Typography>
+              <Box > 
+          <Button component="a" href={stall.menu_card} sx={{
+                  background: "linear-gradient(180deg, #FF6AB7 0%, #6AE4FF 100%)",
+                  color: "black",
+                  fontFamily: "Audiowide",
+                  margin:"10px"
+                }}>View Menu</Button>
+              
+            <Typography sx={{...styles.subHeading,fontSize:"20px",color:"gold",marginBottom:"2%"}}>Items Available:</Typography>
+            <Box sx={{display:"flex",gap:"10px",alignItems:"center",flexDirection:isSmallScreen ? "column" : "row",flexWrap:"wrap"}}>
+              
+            { stall.items && stall.items.split(",").map((item)=>(
+              <Chip label={item} variant="outlined" color="secondary" sx={styles.subHeading} />
+            
+            ))
+            }
+            </Box>
+          </Box>
+            </Box>
+            <Box>
+              
+              <Typography sx={styles.content}>
+                Timings:{stall.timings}
+                </Typography>
+              <Typography sx={styles.content}>
+                Owned By :{stall.belongTo}
+              </Typography>
+              
+            </Box>
+            <Box >
+              <Box sx={styles.subHeading}>
+                Contact Info
+              </Box>
+              <Box>
+                <Typography sx={styles.content}> <LocalPhoneIcon color="info"/> +91 {stall.contact.mobile}</Typography>
+                <Typography sx={styles.content}> <WhatsAppIcon color="success"/> +91 {stall.contact.whatsApp} </Typography>
+              </Box>
+            </Box>
+          </Box>
+          { !isSmallScreen && (
+          <Box sx={{width:"70%", position: "relative",border: "3px solid",borderImageSlice: 1,
+              borderImageSource: 'linear-gradient(to right, #261E35, #C91C75, #EFE1EE, #E9C0DA, #E4AACC, #C91C75, #261E35)'}}>
+                  <Typography sx={{...styles.subHeading, marginBottom:"0%"}}>Our Specials spotlights:</Typography>
+                <ImageCarousel images={[stall.image,stall.imageLeft,stall.imageRight]} />
+              </Box>
+          )}
+        </Box>
+      </Box>
+    </Box>
+  )
 
 	return (
 		<>
@@ -177,6 +304,8 @@ function StallShowcase() {
 									>
 										View Menu
 									</Button>
+
+
 
 									<Typography
 										sx={{
--- src/pages/Stalls.js
+++ src/pages/Stalls.js
@@ -8,11 +8,53 @@ import icecream from "../assets/images/stalls/ice_cream.avif";
 import shawarma from "../assets/images/stalls/shawarma.jpg";
 import tiffin from "../assets/images/stalls/tiffin.avif";
 import StallCard from "../components/stalls/stallCard";
-import foodCourtMap from "../assets/images/stalls/foodcourtmap.png";
+
+import foodCourtMap from "../assets/images/stalls/foodcourtmap.png"
+
 import { fetchStalls } from "../redux/slices/stallsSlice";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import PageTop from "../components/general/PageTop";
+
+import TechnicalEvents from "./TechnicalEvents";
+import TechnicalShimmer from "../components/technicalEvents/technicalShimmer";
+import { Grid } from "swiper/modules";
+
+
+export default function AllStalls() {
+	const dispatch = useDispatch();
+	
+	const { stalls, loading, error } = useSelector((state) => state.stalls);
+	
+
+
+	const fetchAllStalls = async () => {
+		
+		try {
+		  
+		  dispatch(fetchStalls())
+		} catch (error) {
+		} 
+	
+	  };
+
+	useEffect(() => {
+		
+		  fetchAllStalls();
+		
+	  },[]);
+	
+	  if(loading)
+	  {
+		<Grid container spacing={3} sx={{ marginTop: "20px", marginBottom: "40px", padding: "5px" }}>
+            {[...Array(4)].map((_, index) => (
+              <Grid item xs={12} sm={6} md={3} key={index}>
+                <TechnicalShimmer />
+              </Grid>
+            ))}
+          </Grid>
+	  }
+
 import TechnicalShimmer from "../components/technicalEvents/technicalShimmer";
 import { Grid } from "swiper/modules";
 
@@ -44,6 +86,7 @@ export default function AllStalls() {
 			))}
 		</Grid>;
 	}
+
 	return (
 		<div
 			style={{
@@ -63,6 +106,7 @@ export default function AllStalls() {
 					img6={icecream}
 					text="Stalls"
 				/>
+
 			</div>
 			<Typography
 				variant="h4"
@@ -95,6 +139,9 @@ export default function AllStalls() {
 					</Grid2>
 				))}
 			</Grid2>
+
+			<img src={foodCourtMap}></img>
+
 			<Box
 				sx={{
 					display: "flex",
@@ -135,6 +182,7 @@ export default function AllStalls() {
 					}}
 				/>
 			</Box>
+
 		</div>
 	);
 }
