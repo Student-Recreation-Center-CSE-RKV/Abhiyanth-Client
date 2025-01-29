eb931af6965383cc3023d9cb0da5306b11ab964d
Thota Srikanth <tsrikanth866@gmail.com>
2025-01-29 23:58:24 +0530

    Google authentication for the admin branch was done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { auth } from './api/firebaseConfig';
+
 
 import Admin from './pages/Admin';
 
 import ScrollToTop from './components/general/ScrollToTop';
+import SignIn from './components/login/SignIn';
+import { useEffect, useState } from 'react';
 
+const provider = new GoogleAuthProvider();
 function App() {
 
+ 
+  const [user, setUser] = useState(null)
+
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setUser(user)
+    })
+  })
   return (
     <BrowserRouter>
       <ScrollToTop />
       <Routes>
-        <Route path='/' element={<Admin />}/>
+        <Route path='/' element={<Admin />} />
+        <Route path='/auth/login' element={<SignIn />} />
       </Routes>
     </BrowserRouter>
   );
